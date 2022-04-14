151062cf64940a219bac85b8dcecefb5afb010ce
paul <paul@genesis.websealevel.com*>
2022-04-14 18:16:11 +0200

    refactor - transform methodes en fonction

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
 // var_dump($start->format('H:i'));
 // var_dump($end->format('H:i'));
 
+
+namespace Websealevel\TimeTable;
+
 /**
  * Modéle d'un horaire unitaire (début - fin)
  */
-class WSL_Unit_Timetable {
+class Unit_Timetable {
     /**
 	 * Constructeur.
 	 *
@@ -24,13 +27,13 @@ class WSL_Unit_Timetable {
 	/**
 	 * Retourne l'horaire unitaire formaté
 	 *
-	 * @param WSL_Unit_Timetable l'horaire à formater
+	 * @param Unit_Timetable l'horaire à formater
 	 * @param string $start_end Formater le DateTime du matin ('am'), de l'apres-midi ('am') ou les deux ('am_pm').
 	 * @param string $format Format du DateTime.
 	 * @param string $separator Optional Le séparateur entre l'horaire unitaire du matin et de l'après-midi, si $start_end est égal à 'start_end'.
 	 * @return string
 	 */
-function wsl_format_unit_timetable(WSL_Unit_Timetable $time_table = new WSL_Day_Timetable(), string $start_end = 'start_end', string $format = 'H:i', string $separator = '-' ){
+function wsl_format_Unit_Timetable(Unit_Timetable $time_table = new Day_Timetable(), string $start_end = 'start_end', string $format = 'H:i', string $separator = '-' ){
 
 	return match($start_end){
 		'start' => $time_table->start->format($format),
@@ -41,62 +44,39 @@ function wsl_format_unit_timetable(WSL_Unit_Timetable $time_table = new WSL_Day_
 }
 
 
-var_dump(wsl_format_unit_timetable(new WSL_Unit_Timetable(new DateTimeImmutable('7:0'), new DateTimeImmutable('12:0'))));
+var_dump(wsl_format_Unit_Timetable(new Unit_Timetable(new DateTimeImmutable('7:0'), new DateTimeImmutable('12:0'))));
 
 /**
  * Modèle d'un horaire d'une journée (Horaire unitaire matin, Horaire unitaire après-midi)
  */
-class WSL_Day_Timetable {
-
+class Day_Timetable {
 	/**
 	 * Constructeur.
 	 *
 	 * @param string $label Un label pour identifier la journée (par ex 'lundi')
-	 * @param WSL_Unit_Timetable $am Horaire du matin.
-	 * @param WSL_Unit_Timetable $pm Horaire de l'apres midi.
+	 * @param Unit_Timetable $am Horaire du matin.
+	 * @param Unit_Timetable $pm Horaire de l'apres midi.
 	 */
 	public function __construct(
 		readonly public string $label = '', 
-		readonly public WSL_Unit_Timetable $am = new WSL_Unit_Timetable(),
-		readonly public WSL_Unit_Timetable $pm = new WSL_Unit_Timetable()
+		readonly public Unit_Timetable $am = new Unit_Timetable(),
+		readonly public Unit_Timetable $pm = new Unit_Timetable()
 	) {}
-	// /**
-	//  * Retourne l'horaire formaté
-	//  *
-	//  * @param string $am_pm Formater le DateTime du matin ('am'), de l'apres-midi ('am') ou les deux ('am_pm').
-	//  * @param string $format Format du DateTime.
-	//  * @param string $separator_am_pm Optional Le séparateur entre l'horaire unitaire du matin et de l'après-midi, si $start_end est égal à 'start_end'.
-	//  * @param string $separator_start_end Optional Le séparateur entre le début et la fin si $start_end est égal à 'start_end'.
-	//  * @return string
-	//  */
-	// public function format( string $am_pm = 'am', string $format = 'H:i', string $separator_am_pm = ' ', string $separator_start_end = '-' ): string|array {
-
-	// 	return match ($am_pm) {
-	// 		'am' => $this->am->format( format: $format, separator:$separator_start_end ),
-	// 		'pm' => $this->pm->format( format: $format, separator:$separator_start_end ),
-	// 		'am_pm' => array( 
-    //             'am' => $this->am->format( format: $format, separator:$separator_start_end ),
-    //             'pm' => $this->pm->format( format: $format, separator:$separator_start_end ),
-    //             'separator' => $separator_am_pm
-    //         ), 
-    //         default => 'Not found'
-	// 	};
-	// }
 }
 
 
 	/**
 	 * Retourne l'horaire formaté
 	 *
-	 * @param WSL_Day_Timetable $day L'horaire de journée à formatter
+	 * @param Day_Timetable $day L'horaire de journée à formatter
 	 * @param string $am_pm Formater le DateTime du matin ('am'), de l'apres-midi ('am') ou les deux ('am_pm').
 	 * @param string $format Format du DateTime.
 	 * @param string $separator_am_pm Optional Le séparateur entre l'horaire unitaire du matin et de l'après-midi, si $start_end est égal à 'start_end'.
 	 * @param string $separator_start_end Optional Le séparateur entre le début et la fin si $start_end est égal à 'start_end'.
-	 * @param callable $format_unit_time_table Une fonction pour formater un WSL_Unit_Timetable
+	 * @param callable $format_unit_time_table Une fonction pour formater un Unit_Timetable
 	 * @return string
 	 */
-	 function wsl_format_day_timetable(WSL_Day_Timetable $day = new WSL_Day_Timetable(), callable $format_unit_time_table, string $am_pm = 'am_pm', string $format = 'H:i', string $separator_am_pm = ' ', string $separator_start_end = '-',  ): string|array {
+	 function wsl_format_day_timetable(Day_Timetable $day = new Day_Timetable(), callable $format_unit_time_table, string $am_pm = 'am_pm', string $format = 'H:i', string $separator_am_pm = ' ', string $separator_start_end = '-',  ): string|array {
 
 
 		return match ($am_pm) {
@@ -114,21 +94,18 @@ class WSL_Day_Timetable {
 		};
 	}
 
-	var_dump(wsl_format_day_timetable(day: new WSL_Day_Timetable(), format_unit_time_table: 'wsl_format_unit_timetable'));
+	var_dump(wsl_format_day_timetable(day: new Day_Timetable(), format_unit_time_table: 'wsl_format_Unit_Timetable'));
 
-
-// $lundi = new WSL_Day_Timetable();
-// var_dump($lundi->format(am_pm: 'am_pm',format:'H:i', separator_am_pm:'/', separator_start_end:'-'));
 
 
 $time_table = array(
-	new WSL_Day_Timetable(
+	new Day_Timetable(
 		'lundi', 
-		new WSL_Unit_Timetable(
+		new Unit_Timetable(
 			new DateTimeImmutable('8:0'), 
 			new DateTimeImmutable('12:0')
 		),
-		new WSL_Unit_Timetable(
+		new Unit_Timetable(
 			new DateTimeImmutable('13:30'), 
 			new DateTimeImmutable('17:0')
 		)),
@@ -136,14 +113,14 @@ $time_table = array(
 
 /**
  * Retourne un tableau des horaires formatés
- * @param WSL_Day_Timetable[] $timeTable
+ * @param Day_Timetable[] $timeTable
  */
 function show_time_table(array $timeTable = array()){
 
 	$result = array();
 
 	foreach($timeTable as $day){
-		$result[$day->label] = wsl_format_day_timetable($day, 'wsl_format_unit_timetable');
+		$result[$day->label] = wsl_format_day_timetable($day, 'wsl_format_Unit_Timetable');
 	}
 
 	return $result;
