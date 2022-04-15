7eef7e946dc22f7cfca0b7346996d8a4846339de
paul <paul@genesis.websealevel.com*>
2022-04-15 18:20:29 +0200

    wip add phpunit tests suite

---
+++ tests/FormattingTest.php
@@ -0,0 +1,10 @@
+<?php
+
+// require dirname(__DIR__) . '/src/Models/DayTimetable.php';
+// require dirname(__DIR__) . '/src/Models/UnitTimetable.php';
+// require dirname(__DIR__) . '/src/utils.php';
+
+
+class FormattingTest extends PHPUnit_Framework_TestCase
+{
+}
